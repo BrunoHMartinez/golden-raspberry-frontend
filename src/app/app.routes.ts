114bde12180363d12887547c5c2088d946eeb417
BrunoHMartinez <bruno.h_m@hotmail.com>
2025-12-05 13:21:18 -0300

    Creation of components and config routers

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', loadComponent: () => import('./pages/dashboard/dashboard').then(m => m.Dashboard) },
+  { path: 'movies', loadComponent: () => import('./pages/movies/movies-list/movies-list').then(m => m.MoviesList) },
+  { path: '**', redirectTo: 'dashboard' }
+];
