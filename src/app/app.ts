c44a0a4612a827a05deee588ff69a8f5c5975d44
BrunoHMartinez <bruno.h_m@hotmail.com>
2025-12-05 14:29:21 -0300

    implemantation of header component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './layout/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  imports: [RouterOutlet, Header],
+  template: `
+    <app-header></app-header>
+    <main class="container">
+      <router-outlet></router-outlet>
+    </main>
+  `
 })
-export class App {
-  protected readonly title = signal('golden-raspberry-frontend');
-}
+export class App {}
